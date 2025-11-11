9858e340a33a0e5bd794a395af94addae8a0fc8b
Nathan Simon <professorgottem@gmail.com>
2025-11-11 13:21:33 -0500

    rendering stuff gets its own folder now

--- common/include/kernel/direct_text_render.hpp
+++ common/include/kernel/direct_text_render.hpp
@@ -4,11 +4,11 @@
 #include "string"
 class direct_text_render
 {
-	uint32_t* __fb_ptr      {};
-	font_render __render    {};
-	uint32_t __fb_wid       {};
-	uint32_t __fb_ht        {};
-	point __cursor_pos      { 0, 0 };
+	uint32_t* __fb_ptr;
+	font_render __render;
+	uint32_t __fb_wid;
+	uint32_t __fb_ht;
+	point __cursor_pos;
 	constexpr uint32_t __fb_col_cap() const { return __fb_wid / __render.glyph_width(); }
 	constexpr uint32_t __fb_row_cap() const { return __fb_ht / __render.glyph_height(); }
 	void __advance() noexcept;
@@ -16,11 +16,12 @@ class direct_text_render
 public:
 	constexpr direct_text_render() noexcept = default;
 	constexpr direct_text_render(sysinfo_t* fb_info, psf2_t* font_data, uint32_t fg_color, uint32_t bg_color) noexcept :
-		__fb_ptr    { fb_info->fb_ptr },
-		__render    { font_data, fb_info->fb_pitch, fg_color, bg_color },
-		__fb_wid    { fb_info->fb_width },
-		__fb_ht     { fb_info->fb_height }
-					{}
+		__fb_ptr    	{ fb_info->fb_ptr },
+		__render    	{ font_data, fb_info->fb_pitch, fg_color, bg_color },
+		__fb_wid    	{ fb_info->fb_width },
+		__fb_ht     	{ fb_info->fb_height },
+		__cursor_pos	{ 0U, 0U }
+						{}
 	constexpr void set_fg_color(uint32_t color) noexcept { __render.set_fg_color(color); }
 	constexpr uint32_t get_fg_color() const noexcept { return __render.fg_color(); }
 	constexpr void set_bg_color(uint32_t color) noexcept { __render.set_bg_color(color); }
