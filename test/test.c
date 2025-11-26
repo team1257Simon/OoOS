bad3b51b229dd3a018b5b6bc19c758aa873a4380
Nathan Simon <professorgottem@gmail.com>
2025-11-25 21:27:29 -0500

    whoops, missed a spot

--- test/test.c
+++ test/test.c
@@ -7,19 +7,19 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-int	mkdir (const char *_path, mode_t __mode);
+int	mkdir(const char *_path, mode_t __mode);
 int main(int argc, char** argv)
 {
 	printf("Hello OoOS (from userland)! I'm %s!\n", argv[0]);
-	uint64_t tm = time(NULL);
+	uint64_t tm			= time(NULL);
 	printf("The timestamp is %lu\n", tm);
-	char* test_str = (char*)mmap(NULL, 4096, PROT_WRITE | PROT_READ, MAP_ANONYMOUS, 0, 0);
+	char* test_str		= (char*)mmap(NULL, 4096, PROT_WRITE | PROT_READ, MAP_ANONYMOUS, 0, 0);
 	if((void*)test_str != (void*)-1)
 	{
-		int len = snprintf(test_str, 4096, "Address of buffer: %p; number of arguments is %i\n", (void*)test_str, argc);
-		char* copied = (char*)malloc(len + 1);
+		int len			= snprintf(test_str, 4096, "Address of buffer: %p; number of arguments is %i\n", (void*)test_str, argc);
+		char* copied	= (char*)malloc(len + 1);
 		strncpy(copied, test_str, len);
-		copied[len] = 0;
+		copied[len]		= 0;
 		printf("%s", copied);
 		free(copied);
 		munmap(test_str, 4096);
@@ -29,12 +29,12 @@ int main(int argc, char** argv)
 		printf("mmap failed; error code %i\n", errno);
 		return errno;
 	}
-	FILE* f = fopen("/files/memes.txt", "a+");
+	FILE* f				= fopen("/files/memes.txt", "a+");
 	if(f)
 	{
 		fprintf(f, "blerple derple\n");
 		fclose(f);
-		f = fopen("/files/dreams.txt", "w+");
+		f				= fopen("/files/dreams.txt", "w+");
 		if(f) { fprintf(f, "merple yerple\n"); fclose(f); }
 		else printf("no file :(\n");
 	}
@@ -42,13 +42,13 @@ int main(int argc, char** argv)
 	if((mkdir("/files/fleedledeedle", 0774) < 0) && errno != EEXIST) printf("no directory creation q.q\n");
 	else
 	{
-		FILE* g = fopen("/files/fleedledeedle/flaboogle.txt", "w+");
+		FILE* g			= fopen("/files/fleedledeedle/flaboogle.txt", "w+");
 		if(g) { fprintf(g, "sha-dizzle!\n"); fclose(g); }
 		else printf("no file ;-;\n");
 	}
-	long pid = fork();
+	long pid			= fork();
 	printf("forked\n");
-	int rv = 0;
+	int rv				= 0;
 	switch(pid)
 	{
 	case -1:
