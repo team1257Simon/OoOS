12cc903ca279b54442874eaf1deda582cdf3b295
Nathan Simon <professorgottem@gmail.com>
2025-12-16 22:09:07 -0500

    tabs!

--- lib/gcc/config/ooos.h
+++ lib/gcc/config/ooos.h
@@ -16,12 +16,12 @@
 
 /* Additional predefined macros. */
 #undef  TARGET_OS_CPP_BUILTINS
-#define TARGET_OS_CPP_BUILTINS()    \
-do {                                \
-	builtin_define("__ooos__");      \
-	builtin_define("__unix__");      \
-	builtin_assert("system=ooos");   \
-	builtin_assert("system=unix");   \
+#define TARGET_OS_CPP_BUILTINS()	\
+do {								\
+	builtin_define("__ooos__");		\
+	builtin_define("__unix__");		\
+	builtin_assert("system=ooos");	\
+	builtin_assert("system=unix");	\
 } while(0);
 
 #undef  LINK_SPEC
