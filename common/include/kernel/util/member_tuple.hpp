bcfa72e5c983e7fcceb4bc475c6dbcc3c411ac67
Nathan Simon <professorgottem@gmail.com>
2026-05-17 16:39:11 -0400

    member_tuple now doesn't fail to compile when a class has static data members

--- common/include/kernel/util/member_tuple.hpp
+++ common/include/kernel/util/member_tuple.hpp
@@ -12,7 +12,7 @@ namespace ooos
 		{
 			consteval static bool __is_pointable(std::meta::info i)
 			{
-				if(std::meta::is_special_member_function(i) || std::meta::is_template(i) || std::meta::is_constructor(i) || std::meta::is_destructor(i))
+				if(std::meta::is_special_member_function(i) || std::meta::is_template(i) || !std::meta::has_linkage(i))
 					return false;
 				else try { return (std::meta::type_of(i), true); } catch(...) { return false; }
 			}
@@ -37,9 +37,16 @@ namespace ooos
 						| std::views::filter(__is_pointable)
 						| std::ranges::to<std::vector>())[I];
 				}
-				using __base_type = typename [: std::meta::type_of(__get()) :];
+				constexpr static bool __is_static	= std::meta::is_static_member(__get());
+				using __base_type 					= typename [: std::meta::type_of(__get()) :];
 				typedef __maybe_member_t<std::meta::is_static_member(__get()), __base_type> __type;
-				constexpr static __type __value = std::meta::extract<__type>(__get());
+				constexpr static __type __get_value()
+				{
+					if constexpr(__is_static && std::is_object_v<__base_type>)
+						return &[:__get():];
+					else return std::meta::extract<__type>(__get());
+				}
+				constexpr static __type __value = __get_value();
 			};
 			template<size_t ... Is>	consteval static auto __get(std::index_sequence<Is...>) { return std::make_tuple(__member_at_index<__count - Is - 1>::__value...); }
 			typedef std::make_index_sequence<__count_members()> __indices;
